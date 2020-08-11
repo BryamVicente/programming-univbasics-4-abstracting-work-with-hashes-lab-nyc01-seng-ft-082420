9c1d2e7a65c8819534131976ffbb3d1b5409ec27
Bryam Vicente <vicentebryam@gmail.com>
2020-08-11 22:10:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,7 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  key = name
+  value = "Grace Hopper"
   hash = {:key => value}
 end
 
